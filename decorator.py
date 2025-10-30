e7513e807f8758e1c5298e18ac4b671d17a091d5
Eric Zhou <yiruiericzhou@gmail.com>
2025-10-30 19:39:36 +0800

    add override decorator

--- decorator.py
+++ decorator.py
@@ -5,8 +5,9 @@ Provides commonly used decorators and metaclasses
 
 def singleton(cls):
     """
+    Singleton decorator for classes.
     Use this if the class itself do not need to be referenced in the future.
-    This will convert the class to a function that returns the singleton instance
+    This will convert the class to a function that returns the singleton instance.
     Syntax is as follows:
     ```
     @decorator.singleton
@@ -27,6 +28,7 @@ def singleton(cls):
 
 class Singleton(type):
     """
+    Singleton metaclass for classes.
     Use this if the class itself is still needed.
     Syntax is as follows:
     ```
@@ -43,3 +45,24 @@ class Singleton(type):
         if cls not in cls._instances:
             cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
+
+
+def overrides(interface_class):
+    """
+    Use this to indicate that a method is overriding a method in the superclass.
+    This is useful for static type checkers to ensure that the method signature matches.
+    Syntax is as follows:
+    ```
+    class <subclass>(<superclass>):
+        @decorator.overrides(<superclass>)
+        def <method>(self, ...):
+            ...
+    ```
+    Reference: https://stackoverflow.com/questions/1167617/in-python-how-do-i-indicate-im-overriding-a-method
+    """
+
+    def overrider(method):
+        assert method.__name__ in dir(interface_class)
+        return method
+
+    return overrider
