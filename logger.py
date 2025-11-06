23ab5621c1ee557884753404d9bed1dcdc6b64b6
Eric Zhou <yiruiericzhou@gmail.com>
2025-11-06 15:58:24 +0800

    add ability to add logging levels

--- logger.py
+++ logger.py
@@ -370,6 +370,60 @@ class CompLogger:
 
 comp_logger = Logger().register_component(__file__)
 
+
+def addLoggingLevel(levelName, levelNum, methodName=None):
+    """
+    Comprehensively adds a new logging level to the `logging` module and the
+    currently configured logging class.
+
+    `levelName` becomes an attribute of the `logging` module with the value
+    `levelNum`. `methodName` becomes a convenience method for both `logging`
+    itself and the class returned by `logging.getLoggerClass()` (usually just
+    `logging.Logger`). If `methodName` is not specified, `levelName.lower()` is
+    used.
+
+    To avoid accidental clobberings of existing attributes, this method will
+    raise an `AttributeError` if the level name is already an attribute of the
+    `logging` module or if the method name is already present 
+
+    Example
+    -------
+    >>> addLoggingLevel('TRACE', logging.DEBUG - 5)
+    >>> logging.getLogger(__name__).setLevel("TRACE")
+    >>> logging.getLogger(__name__).trace('that worked')
+    >>> logging.trace('so did this')
+    >>> logging.TRACE
+    5
+
+    Reference: https://stackoverflow.com/questions/2183233/how-to-add-a-custom-loglevel-to-pythons-logging-facility/35804945#35804945
+    """
+    if not methodName:
+        methodName = levelName.lower()
+
+    if hasattr(logging, levelName):
+       raise AttributeError('{} already defined in logging module'.format(levelName))
+    if hasattr(logging, methodName):
+       raise AttributeError('{} already defined in logging module'.format(methodName))
+    if hasattr(logging.getLoggerClass(), methodName):
+       raise AttributeError('{} already defined in logger class'.format(methodName))
+
+    # This method was inspired by the answers to Stack Overflow post
+    # http://stackoverflow.com/q/2183233/2988730, especially
+    # http://stackoverflow.com/a/13638084/2988730
+    def logForLevel(self, message, *args, **kwargs):
+        if self.isEnabledFor(levelNum):
+            self._log(levelNum, message, args, **kwargs)
+    def logToRoot(message, *args, **kwargs):
+        logging.log(levelNum, message, *args, **kwargs)
+
+    logging.addLevelName(levelNum, levelName)
+    setattr(logging, levelName, levelNum)
+    setattr(logging.getLoggerClass(), methodName, logForLevel)
+    setattr(logging, methodName, logToRoot)
+
+addLoggingLevel("VERBOSE1", logging.INFO - 1, "verbose1")
+addLoggingLevel("VERBOSE2", logging.INFO - 2, "verbose2")
+
 import re
 import sys
 import atexit
@@ -411,13 +465,6 @@ def exc_handler(exctype, value, tb):
 
 # register the custom exception handler with system
 sys.excepthook = exc_handler
-
-
-def log_time_breakdown(tag: str):
-    time_ns = time.monotonic_ns()
-    with open(os.path.join(Logger().log_dirpath, "time_break_down.txt"), "a") as fout:
-        fout.write(f"{tag}, {time_ns}\n")
-    return
 
 
 def save_config_to_log_dir(config_path: str):
